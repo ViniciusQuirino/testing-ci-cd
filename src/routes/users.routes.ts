b47ed305ae9e4c8dc99ad50ae24149865d4cec17
Leandro <leanndrocabral@gmail.com>
2023-04-05 14:16:34 -0300

    Create user post. Pending.

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,8 +2,8 @@ import express from "express";
 
 const users = express.Router();
 
-users.get("/", (req, res) => {
-  return res.status(200).json("Hello World!");
+users.post("/", (req, res) => {
+  
 });
 
 export default users;
