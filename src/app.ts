9c928d7c5544f11fdd68c38ce0baf422d27de3a5
m4chado <enzo653fm@gmail.com>
2023-04-05 23:09:53 -0300

    Structure of folders and files reorganized.

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
-import "reflect-metadata";
 import express from "express";
+import "reflect-metadata";
 import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
 import userRoutes from "./routes/users/users.routes";
+import sessionRoutes from "./routes/session/session.routes";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/session", sessionRoutes);
 app.use("/users", userRoutes);
 
 app.use(handleErrorMiddleware);
