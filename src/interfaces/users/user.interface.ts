6663f4f38f7c1015796c7bcc4d4abd005df4fd3b
m4chado <enzo653fm@gmail.com>
2023-04-05 22:15:37 -0300

    Created users interfaces

---
+++ src/interfaces/users/user.interface.ts
@@ -0,0 +1,55 @@
+import { number } from "yup";
+import { IAddressCreateRequest } from "../adresses/address.interface";
+
+export interface IUserLogin {
+	email: string;
+	password: string;
+}
+
+export interface IUserCreateRequest {
+	name: string;
+	cpf: string;
+	email: string;
+	password: string;
+	phone_number: string;
+	description?: string;
+	birth_date: Date;
+	image_url: string;
+	is_seller: boolean;
+	address: IAddressCreateRequest;
+}
+
+export interface IUserUpdateRequest {
+	name?: string;
+	cpf?: string;
+	email?: string;
+	password?: string;
+	phone_number?: string;
+	description?: string;
+	birth_date?: Date;
+	image_url?: string;
+	is_seller?: boolean;
+}
+
+export interface IUserWithAddressResponse {
+	name: string;
+	cpf: string;
+	email: string;
+	phone_number: string;
+	description?: string;
+	birth_date: Date;
+	image_url: string;
+	is_seller: boolean;
+	address: IAddressCreateRequest;
+}
+
+export interface IUserResponse {
+	name: string;
+	cpf: string;
+	email: string;
+	phone_number: string;
+	description?: string;
+	birth_date: Date;
+	image_url: string;
+	is_seller: boolean;
+}
