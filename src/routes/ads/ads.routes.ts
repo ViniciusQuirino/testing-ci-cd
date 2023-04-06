6925ce4bfbf007ff8a4c011baf4489d4831448ef
Leandro <leanndrocabral@gmail.com>
2023-04-06 18:05:03 -0300

    Created ads DELETE route. Working.

--- src/routes/ads/ads.routes.ts
+++ src/routes/ads/ads.routes.ts
@@ -2,7 +2,9 @@ import express from "express";
 import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
 import { createAdsController } from "../../controllers/ads/createAds.controller";
 import { adsCreateRequestSerializer } from "../../serializers/ads/ads.serializer";
+import { deleteAdsController } from "../../controllers/ads/deleteAds.controller";
 
 export const adsRoutes = express.Router();
 
 adsRoutes.post("", ensureDataIsValidMiddleware(adsCreateRequestSerializer), createAdsController);
+adsRoutes.delete("/:id",deleteAdsController);
---
+++ src/services/ads/deleteAds.service.ts
@@ -0,0 +1,7 @@
+import { prisma } from "../../prisma";
+
+export const deleteAdsService = async (id: string) => {
+  await prisma.car.delete({ where: { id } });
+  
+  return;
+};
