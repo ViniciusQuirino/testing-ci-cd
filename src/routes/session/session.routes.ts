65a1aa4da5a0fea5a96ee457359e670588d91ced
m4chado <enzo653fm@gmail.com>
2023-04-05 23:02:37 -0300

    Added middlewares in the routes that without.

--- src/routes/session/session.routes.ts
+++ src/routes/session/session.routes.ts
@@ -1,8 +1,14 @@
 import express from "express";
 import { createSessionController } from "../../controllers/session/createSession.controller";
+import ensureDataIsValidMiddleware from "../../middlewares/ensureDataIsValid.middleware";
+import { sessionRequestSerializer } from "../../serializers/session/session.serializer";
 
 const sessionRoutes = express.Router();
 
-sessionRoutes.post("", createSessionController);
+sessionRoutes.post(
+	"",
+	ensureDataIsValidMiddleware(sessionRequestSerializer),
+	createSessionController
+);
 
 export default sessionRoutes;
--- src/routes/users/users.routes.ts
+++ src/routes/users/users.routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { createUserController } from "../../controllers/users/createUser.controller";
+import { listUsersController } from "../../controllers/users/listUsers.controller";
+import ensureDataIsValidMiddleware from "../../middlewares/ensureDataIsValid.middleware";
+import { userCreateRequestSerializer } from "../../serializers/users/user.serializer";
 
 const userRoutes = express.Router();
 
-userRoutes.post("", createUserController);
+userRoutes.post("", ensureDataIsValidMiddleware(userCreateRequestSerializer), createUserController);
+userRoutes.get("/all", listUsersController);
 
 export default userRoutes;
