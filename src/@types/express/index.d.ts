313747afd38d6365fbfce3f79a2a4c6104798d9a
m4chado <enzo653fm@gmail.com>
2023-04-05 23:47:39 -0300

    Created Permission Middlewares

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,7 +1,10 @@
 import * as express from "express";
+import { IUserWithAddressResponse } from "../../interfaces/users/user.interface";
 
 declare global {
 	namespace Express {
-		interface Request {}
+		interface Request {
+			user: IUserWithAddressResponse;
+		}
 	}
 }
