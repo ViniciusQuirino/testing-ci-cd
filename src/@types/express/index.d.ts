afdadca3d466ecb108ced6a2e712060e2c1de3b7
m4chado <enzo653fm@gmail.com>
2023-04-10 12:02:23 -0300

    Created updated ads route. Working.

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -1,12 +1,12 @@
 import * as express from "express";
 import { IUserResponse } from "../../interfaces/users/user.interface";
 import { IAdsResponse } from "../../interfaces/ads/ads.interface";
+import { Car } from "@prisma/client";
 
 declare global {
 	namespace Express {
 		interface Request {
 			user: IUserResponse;
-			ads: IAdsResponse;
 		}
 	}
 }
