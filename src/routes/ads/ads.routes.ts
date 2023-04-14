858678982323c192e60fda1eff010a2a4e5c042e
Leandro <leanndrocabral@gmail.com>
2023-04-14 13:16:42 -0300

    Added route protection middleware. Ready.

--- src/routes/ads/ads.routes.ts
+++ src/routes/ads/ads.routes.ts
@@ -2,22 +2,37 @@ import express from "express";
 import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
 import { createAdsController } from "../../controllers/ads/createAds.controller";
 import {
-	adsCreateRequestSerializer,
-	adsUpdateRequestSerializer,
+  adsCreateRequestSerializer,
+  adsUpdateRequestSerializer,
 } from "../../serializers/ads/ads.serializer";
 import { deleteAdsController } from "../../controllers/ads/deleteAds.controller";
 import { listAdsController } from "../../controllers/ads/listAds.controller";
 import { ensureAdsExistsMiddleware } from "../../middlewares/ensureAdsExists.middleware";
 import { updateAdsController } from "../../controllers/ads/updateAds.controller";
+import { ensureAuthMiddleware } from "../../middlewares/ensureAuth.middleware";
+import { ensureIsOwnerOfAdsOrAdm } from "../../middlewares/ensureIsOwnerOfAdsOrAdm.middleware";
 
 export const adsRoutes = express.Router();
 
-adsRoutes.post("", ensureDataIsValidMiddleware(adsCreateRequestSerializer), createAdsController);
-adsRoutes.delete("/:id", ensureAdsExistsMiddleware, deleteAdsController);
+adsRoutes.post(
+  "",
+  ensureAuthMiddleware,
+  ensureDataIsValidMiddleware(adsCreateRequestSerializer),
+  createAdsController
+);
+adsRoutes.delete(
+  "/:id",
+  ensureAuthMiddleware,
+  ensureAdsExistsMiddleware,
+  ensureIsOwnerOfAdsOrAdm,
+  deleteAdsController
+);
 adsRoutes.get("", listAdsController);
 adsRoutes.patch(
-	"/:id",
-	ensureDataIsValidMiddleware(adsUpdateRequestSerializer),
-	ensureAdsExistsMiddleware,
-	updateAdsController
+  "/:id",
+  ensureAuthMiddleware,
+  ensureDataIsValidMiddleware(adsUpdateRequestSerializer),
+  ensureAdsExistsMiddleware,
+  ensureIsOwnerOfAdsOrAdm,
+  updateAdsController
 );
