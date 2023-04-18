c875852f36d20e952b0562a5bb708490cfdfbe89
Leandro <leanndrocabral@gmail.com>
2023-04-18 14:36:59 -0300

    Refactored code and implemented type middleware. Working

--- src/routes/ads/ads.routes.ts
+++ src/routes/ads/ads.routes.ts
@@ -2,22 +2,23 @@ import express from "express";
 import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
 import { createAdsController } from "../../controllers/ads/createAds.controller";
 import {
-	adsCreateRequestSerializer,
-	adsUpdateRequestSerializer,
+  adsCreateRequestSerializer,
+  adsUpdateRequestSerializer,
 } from "../../serializers/ads/ads.serializer";
 import { deleteAdsController } from "../../controllers/ads/deleteAds.controller";
 import { listAdsController } from "../../controllers/ads/listAds.controller";
 import { ensureAdsExistsMiddleware } from "../../middlewares/ensureAdsExists.middleware";
 import { updateAdsController } from "../../controllers/ads/updateAds.controller";
+import { ensureAdsQueryType } from "../../middlewares/ensureAdsQueryType.middleware";
 
 export const adsRoutes = express.Router();
 
 adsRoutes.post("", ensureDataIsValidMiddleware(adsCreateRequestSerializer), createAdsController);
 adsRoutes.delete("/:id", ensureAdsExistsMiddleware, deleteAdsController);
-adsRoutes.get("", listAdsController);
+adsRoutes.get("", ensureAdsQueryType, listAdsController);
 adsRoutes.patch(
-	"/:id",
-	ensureDataIsValidMiddleware(adsUpdateRequestSerializer),
-	ensureAdsExistsMiddleware,
-	updateAdsController
+  "/:id",
+  ensureDataIsValidMiddleware(adsUpdateRequestSerializer),
+  ensureAdsExistsMiddleware,
+  updateAdsController
 );
--- src/services/ads/listAds.service.ts
+++ src/services/ads/listAds.service.ts
@@ -2,17 +2,16 @@ import { IAdsQueries } from "../../interfaces/ads/ads.interface";
 import { prisma } from "../../prisma";
 
 export const listAdsService = async (queries: IAdsQueries, outherQueries: IAdsQueries) => {
-  const { min_km, max_km, min_price, max_price, limit, page } = queries;
+  const { min_km, max_km, min_price, max_price, limit = 12, page = 1 } = queries;
 
   const adsList = await prisma.car.paginate(
     {
       where: {
-        AND:
-          [
-            outherQueries,
-            { km: { gte: min_km, lte: max_km } },
-            { price: { gte: min_price, lte: max_price } },
-          ] ?? undefined,
+        AND: [
+          outherQueries,
+          { km: { gte: min_km, lte: max_km } },
+          { price: { gte: min_price, lte: max_price } },
+        ],
       },
       select: {
         ...prisma.$exclude("car", ["user_id"]),
@@ -21,7 +20,7 @@ export const listAdsService = async (queries: IAdsQueries, outherQueries: IAdsQu
         user: { select: prisma.$exclude("user", ["cpf", "password"]) },
       },
     },
-    { limit: limit ? limit : 12, page: page ? page : 1 }
+    { limit: limit, page: page }
   );
 
   return adsList;
