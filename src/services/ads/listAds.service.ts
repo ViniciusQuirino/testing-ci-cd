40801b4762d1c76f8d1918adf20ca14db6cc1898
Anderson-Leandro <andersonmendesleandro@gmail.com>
2023-04-26 22:38:06 -0300

    fix: query for model ajust

--- src/services/ads/listAds.service.ts
+++ src/services/ads/listAds.service.ts
@@ -1,27 +1,42 @@
 import { IAdsQueries } from "../../interfaces/ads/ads.interface";
 import { prisma } from "../../prisma";
 
-export const listAdsService = async (queries: IAdsQueries, outherQueries: IAdsQueries) => {
-  const { min_km, max_km, min_price, max_price, limit = 12, page = 1 } = queries;
+export const listAdsService = async (
+	queries: IAdsQueries,
+	outherQueries: IAdsQueries
+) => {
+	const {
+		min_km,
+		max_km,
+		min_price,
+		max_price,
+		limit = 12,
+		page = 1,
+	} = queries;
 
-  const adsList = await prisma.car.paginate(
-    {
-      where: {
-        AND: [
-          outherQueries,
-          { km: { gte: min_km, lte: max_km } },
-          { price: { gte: min_price, lte: max_price } },
-        ],
-      },
-      select: {
-        ...prisma.$exclude("car", ["user_id"]),
-        images: { select: prisma.$exclude("image", ["car_id"]) },
-        comments: { select: prisma.$exclude("comments", ["car_id"]) },
-        user: { select: prisma.$exclude("user", ["cpf", "password"]) },
-      },
-    },
-    { limit: limit, page: page }
-  );
+	const model = outherQueries.model;
 
-  return adsList;
+	delete outherQueries.model;
+
+	const adsList = await prisma.car.paginate(
+		{
+			where: {
+				AND: [
+					outherQueries,
+					{ model: { contains: model, mode: "insensitive" } },
+					{ km: { gte: min_km, lte: max_km } },
+					{ price: { gte: min_price, lte: max_price } },
+				],
+			},
+			select: {
+				...prisma.$exclude("car", ["user_id"]),
+				images: { select: prisma.$exclude("image", ["car_id"]) },
+				comments: { select: prisma.$exclude("comments", ["car_id"]) },
+				user: { select: prisma.$exclude("user", ["cpf", "password"]) },
+			},
+		},
+		{ limit: limit, page: page }
+	);
+
+	return adsList;
 };
