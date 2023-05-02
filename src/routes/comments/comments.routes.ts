f0f23874afc4956b3f999e4f1fbd078049082a66
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-05-02 14:55:00 -0300

    fix comments routes. Ready.

--- src/routes/comments/comments.routes.ts
+++ src/routes/comments/comments.routes.ts
@@ -6,16 +6,22 @@ import { updateCommentsController } from "../../controllers/comments/updateComme
 import { listAdCommentsController } from "../../controllers/comments/listComments.controller";
 import { ensureCommentExistsMiddleware } from "../../middlewares/ensureCommentExistsMiddleware";
 import { ensureUserMatchesCommentOwner } from "../../middlewares/ensureUserMatchesCommentOwner";
+import { ensureDataIsValidMiddleware } from "../../middlewares/ensureDataIsValid.middleware";
+import { commentRequestSerializer } from "../../serializers/comments/comments.serializer";
+import { ensureAdsExistsMiddleware } from "../../middlewares/ensureAdsExists.middleware";
 
 export const commentsRoutes = Router();
 
 commentsRoutes.post(
     "/:id", 
     ensureAuthMiddleware, 
+    ensureDataIsValidMiddleware(commentRequestSerializer),
+    ensureAdsExistsMiddleware,
     createCommentsController
 );
 commentsRoutes.get(
     "/:id",
+    ensureAdsExistsMiddleware,
     listAdCommentsController
 );
 commentsRoutes.patch(
@@ -23,6 +29,7 @@ commentsRoutes.patch(
     ensureAuthMiddleware,
     ensureCommentExistsMiddleware,
     ensureUserMatchesCommentOwner,
+    ensureDataIsValidMiddleware(commentRequestSerializer),
     updateCommentsController
 );
 commentsRoutes.delete(
