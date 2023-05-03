0ad84b13e88c0f0b0819cbc71a2ebe436491ae53
Anderson-Leandro <andersonmendesleandro@gmail.com>
2023-05-03 17:39:45 -0300

    fix: add is_active for ads update

--- src/serializers/ads/ads.serializer.ts
+++ src/serializers/ads/ads.serializer.ts
@@ -8,75 +8,82 @@ import {
 	IUniqueAds,
 } from "../../interfaces/ads/ads.interface";
 
-export const adsCreateRequestSerializer: SchemaOf<IAdsCreateRequest> = yup.object().shape({
-	brand: yup.string().required(),
-	car_color: yup.string().required(),
-	fuel_type: yup.string().required(),
-	description: yup.string().required(),
-	km: yup.number().required(),
-	launch_year: yup.number().required(),
-	model: yup.string().required(),
-	price: yup.number().required(),
-	images: yup.object().shape({
-		main_image: yup.string().required(),
-		image_one: yup.string().required(),
-		image_two: yup.string().required(),
-		image_three: yup.string().notRequired(),
-		image_four: yup.string().notRequired(),
-		image_five: yup.string().notRequired(),
-	}),
-});
+export const adsCreateRequestSerializer: SchemaOf<IAdsCreateRequest> = yup
+	.object()
+	.shape({
+		brand: yup.string().required(),
+		car_color: yup.string().required(),
+		fuel_type: yup.string().required(),
+		description: yup.string().required(),
+		km: yup.number().required(),
+		launch_year: yup.number().required(),
+		model: yup.string().required(),
+		price: yup.number().required(),
+		images: yup.object().shape({
+			main_image: yup.string().required(),
+			image_one: yup.string().required(),
+			image_two: yup.string().required(),
+			image_three: yup.string().notRequired(),
+			image_four: yup.string().notRequired(),
+			image_five: yup.string().notRequired(),
+		}),
+	});
 
-export const adsUpdateRequestSerializer: SchemaOf<IAdsUpdateRequest> = yup.object().shape({
-	brand: yup.string(),
-	car_color: yup.string(),
-	fuel_type: yup.string(),
-	description: yup.string(),
-	km: yup.number().required(),
-	launch_year: yup.number(),
-	model: yup.string(),
-	price: yup.number(),
-	images: yup
-		.object()
-		.shape({
-			main_image: yup.string(),
-			image_one: yup.string(),
-			image_two: yup.string(),
-			image_three: yup.string(),
-			image_four: yup.string(),
-			image_five: yup.string(),
-		})
-		.nullable(),
-});
+export const adsUpdateRequestSerializer: SchemaOf<IAdsUpdateRequest> = yup
+	.object()
+	.shape({
+		brand: yup.string(),
+		car_color: yup.string(),
+		fuel_type: yup.string(),
+		description: yup.string(),
+		km: yup.number(),
+		launch_year: yup.number(),
+		model: yup.string(),
+		price: yup.number(),
+		images: yup
+			.object()
+			.shape({
+				main_image: yup.string(),
+				image_one: yup.string(),
+				image_two: yup.string(),
+				image_three: yup.string(),
+				image_four: yup.string(),
+				image_five: yup.string(),
+			})
+			.nullable(),
+		is_active: yup.boolean(),
+	});
 
-export const adsResponseSerializer: SchemaOf<IAdsResponse> = yup.object().shape({
-	id: yup.string().required(),
-	brand: yup.string().required(),
-	car_color: yup.string().required(),
-	fuel_type: yup.string().required(),
-	description: yup.string().required(),
-	km: yup.number().required(),
-	launch_year: yup.number().required(),
-	model: yup.string().required(),
-	price: yup.number().required(),
-	is_active: yup.boolean().required(),
-	sold: yup.boolean().required(),
-	images: yup.object().shape({
+export const adsResponseSerializer: SchemaOf<IAdsResponse> = yup
+	.object()
+	.shape({
 		id: yup.string().required(),
-		main_image: yup.string().required(),
-		image_one: yup.string().required(),
-		image_two: yup.string().required(),
-		image_three: yup.string().nullable(),
-		image_four: yup.string().nullable(),
-		image_five: yup.string().nullable(),
-		car_id: yup.string().required(),
-	}),
-	user_id: yup.string().required(),
-	user: yup.object().shape({
-		name: yup.string().required(),
-		image_url: yup.string().required(),
-	}),
-});
+		brand: yup.string().required(),
+		car_color: yup.string().required(),
+		fuel_type: yup.string().required(),
+		description: yup.string().required(),
+		km: yup.number().required(),
+		launch_year: yup.number().required(),
+		model: yup.string().required(),
+		price: yup.number().required(),
+		is_active: yup.boolean().required(),
+		sold: yup.boolean().required(),
+		images: yup.object().shape({
+			id: yup.string().required(),
+			main_image: yup.string().required(),
+			image_one: yup.string().required(),
+			image_two: yup.string().required(),
+			image_three: yup.string().nullable(),
+			image_four: yup.string().nullable(),
+			image_five: yup.string().nullable(),
+			car_id: yup.string().required(),
+		}),
+		user_id: yup.string().required(),
+		user: yup.object().shape({
+			name: yup.string().required(),
+			image_url: yup.string().required(),
+		}),
+	});
 
 export const listUniqueAdSerializer: SchemaOf<IUniqueAds> = yup.object().shape({
 	id: yup.string().required(),
@@ -117,7 +124,9 @@ export const listUniqueAdSerializer: SchemaOf<IUniqueAds> = yup.object().shape({
 	),
 });
 
-export const listAdsResponseSerializer: SchemaOf<IAdsResponse[]> = yup.array(adsResponseSerializer);
+export const listAdsResponseSerializer: SchemaOf<IAdsResponse[]> = yup.array(
+	adsResponseSerializer
+);
 
 export const adsQueriesSerializer: SchemaOf<IAdsQueries> = yup
 	.object()
