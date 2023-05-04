1501149b168f0a1e94b87aad6da04b6ae3d8678f
Anderson-Leandro <andersonmendesleandro@gmail.com>
2023-05-04 13:20:25 -0300

    fix: merge error

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,8 @@ import server from "./app";
 import "dotenv/config";
 
 let port = process.env.PORT || 3333;
+console.log("teste");
 
 server.listen(+port, "0.0.0.0", () => {
-  console.log("Server running in port 3333");
+	console.log("Server running in port 3333");
 });
