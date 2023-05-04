c37541867557ca619311211d35366192a70180bb
Leandro <leanndrocabral@gmail.com>
2023-05-04 12:32:49 -0300

    Added socket io in comments route. Working.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import app from "./app";
+import server from "./app";
 import "dotenv/config";
 
 let port = process.env.PORT || 3333;
 
-app.listen(+port, "0.0.0.0", () => {
+server.listen(+port, "0.0.0.0", () => {
   console.log("Server running in port 3333");
 });
