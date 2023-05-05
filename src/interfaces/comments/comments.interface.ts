8290e6dce63540c5ed9197bd38b5f5fed34bd314
kardashevd <106695001+tsukedev@users.noreply.github.com>
2023-05-05 01:53:54 -0300

    changed intername for better name. Ready.

---
+++ src/interfaces/comments/comments.interface.ts
@@ -0,0 +1,16 @@
+export interface ICommentRequest {
+    description: string;
+    user_id: string;
+}
+
+export interface IComment {
+    id: string;
+    description: string;
+    user_id: string;
+    car_id: string;
+    created_at: Date;
+}
+  
+export interface ICommentUpdate {
+    description?: string;
+}
