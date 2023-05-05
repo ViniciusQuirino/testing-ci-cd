013659f84083e9ba4f4f90a9f9bf784c5614c2a8
Leandro <leanndrocabral@gmail.com>
2023-05-05 11:23:40 -0300

    Implemented rooms in socket.io. Working.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import server from "./app";
 import "dotenv/config";
 
-let port = process.env.PORT || 3333;
-console.log("teste");
+const port = process.env.PORT || 3333;
 
-server.listen(+port, "0.0.0.0", () => {
-	console.log("Server running in port 3333");
+server.listen(port, () => {
+	console.log(`Server running in port ${port}`);
 });
